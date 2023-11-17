e5985b136ccdf2bbdbee5b9b23c6b7c7650ce539
isabelle luu <isabelleluu0w0@gmail.com>
2023-11-17 11:48:28 -0800

    updated TreeNode class

--- src/trees/FamilyTree.java
+++ src/trees/FamilyTree.java
@@ -69,7 +69,7 @@ public class FamilyTree
             // the nodes of a tree is like traversing a linked list. If that isn’t clear,
             // draw a tree, mark any leaf node, and then mark its ancestors in order from
             // recent to ancient. Expect a question about this on the final exam.
-            TreeNode 
+            for (TreeNode child : children) {
             	if (child.parent != null) {
             		ancestors.add(child.parent);
             	}
